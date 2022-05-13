db1af2ed24a07390e03fa1f5a46362e60b3369c0
Angel Sanchez <dual.angel.sanchez@gmail.com>
2022-05-13 11:26:13 -0500

    Etapa 5 del proyecto

---
+++ Etapa5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreEscuela.Entidades;
+using CoreEscuela.util;
+using static System.Console;
+
+namespace CoreEscuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
+            Printer.EscribirTitulo("Bienvenidos a la escuela".ToUpper());
+            Printer.Beep();
+            
+            ImprimirCursosEscuela(engine.Escuela);
+
+            
+        }
+
+
+        private static void ImprimirCursosEscuela(Escuela escuela)
+        {
+            Printer.EscribirTitulo("Curso de la escuela");
+
+            if (escuela?.Cursos != null)
+            {
+                foreach (var Curso in escuela.Cursos)
+                {
+                    Console.WriteLine($"Nombre: {Curso.Nombre}, Id: {Curso.UniqueId}");
+                }
+            }
+
+        }
+
+    }
+}
