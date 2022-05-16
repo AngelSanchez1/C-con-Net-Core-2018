58a3ec25144324009c4188540c8650d20b2bca78
Angel Sanchez <dual.angel.sanchez@gmail.com>
2022-05-16 10:27:27 -0500

    Creacion de la etapa 7 donde usamos la refactorización que es una técnica de la ingeniería de software para reestructurar un código fuente, alterando su estructura interna sin cambiar su comportamiento externo

---
+++ Etapa7/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreEscuela.Entidades;
+using CoreEscuela.util;
+using static System.Console;
+
+namespace CoreEscuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.ProcessExit += AccionDelEvento;
+            AppDomain.CurrentDomain.ProcessExit += (o, s)=>Printer.Beep(2000,1000,1);
+
+            PonerColorDeFondo();
+            //se llama a la clase de EscuelaEngine que es la que inicializa las cargas de cursos, asignaturas y evaluaciones
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
+
+            //se llama a la clase printer que es la cual imprime en consola el titulo de la escuela 
+            //con sus diferentes atributos 
+
+            Printer.EscribirTitulo("Bienvenidos a la escuela".ToUpper());
+            /*Printer.Beep();*/
+            //ImprimirCursosEscuela(engine.Escuela);
+            Dictionary<int, string> diccionario = new Dictionary<int, string>();
+            diccionario.Add(10,"morenaso");
+            diccionario.Add(23,"Juanga");
+
+            foreach( var keyValPair in diccionario)
+            {
+                Console.WriteLine( $"Key: {keyValPair.Key} Valor: {keyValPair.Value}");
+            }
+
+            var dictemp = engine.GetDiccionarioObjeto();
+            engine.ImprimirDiccionario(dictemp,true);
+
+
+        }
+
+        private static void AccionDelEvento(object sender, EventArgs e)
+        {
+            Printer.EscribirTitulo("SALIENDO");
+            Printer.Beep();
+            Printer.EscribirTitulo("SALIO");
+        }
+
+        private static void ImprimirCursosEscuela(Escuela escuela)
+        {
+            Printer.EscribirTitulo("Curso de la escuela");
+
+            if (escuela?.Cursos != null)
+            {
+                foreach (var Curso in escuela.Cursos)
+                {
+                    Console.WriteLine($"Nombre: {Curso.Nombre}, Id: {Curso.UniqueId}");
+                }
+            }
+
+        }
+        //constructor que coloca un fondo de tras de consola y coloca un color a las letras
+        public static void PonerColorDeFondo()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            /*Console.Clear();*/
+        }
+
+    }
+}
