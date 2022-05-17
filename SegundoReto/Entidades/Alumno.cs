6abd68b1061fb572be5ca607e4d12885792c2ee9
Angel Sanchez <dual.angel.sanchez@gmail.com>
2022-05-16 20:47:28 -0500

    Para este reto  cree un diccionario que obtiene el promedio del alumno por asignatura y lo recorre en un foreach para obtener los promedio mas alto

---
+++ SegundoReto/Entidades/Alumno.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace CoreEscuela.Entidades
+{
+    public class Alumno: ObjetoEscuelaBase
+    {
+     
+        public List<Evaluación> Evaluaciones { get; set; } = new List<Evaluación>();
+
+      
+    }
+
+}
